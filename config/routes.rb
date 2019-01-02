bfde6395179d7740a25ec28e2035ac13548a3f42
Ali Fadel <aliosm1997@gmail.com>
2019-01-02 13:03:27 +0200

    Setup bootstrap, bootswatch, jquery-rails, font-awesome-sass, simple_form and devise gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  
   get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
+  
+  devise_for :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
