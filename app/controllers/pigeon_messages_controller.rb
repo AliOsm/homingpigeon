81cf5c4b69ff33c8f4e47cd5f282bfaf73f4f6cd
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2019-05-03 21:20:07 +0300

    Raise the number of pigeon messages

--- app/controllers/pigeon_messages_controller.rb
+++ app/controllers/pigeon_messages_controller.rb
@@ -14,7 +14,7 @@ class PigeonMessagesController < ApplicationController
         render json: { status: :invalid_pigeon_token }, status: :bad_request
       else
         pigeon.pigeon_messages.create(information: information, user: pigeon.user)
-        if pigeon.pigeon_messages_count > 10
+        if pigeon.pigeon_messages_count > 100
           pigeon.pigeon_messages.order(created_at: :desc).last.destroy
         end
         render json: { status: :pigeon_message_created }, status: :created
